f92e6fdf03edd7a40484fe8cedef7b03da4dba70
LOGerasimova <logerasimova5@gmail.com>
2019-12-17 21:00:18 +0700

    TooltipController

---
+++ Rescues/Assets/Scripts/Controller/GameControllers/TooltipControllers.cs
@@ -0,0 +1,41 @@
+﻿namespace Rescues
+{
+    public class TooltipControllers
+    {
+        #region Fields
+
+        private readonly GameContext _context;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public TooltipControllers(GameContext context, Services services)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+
+        #region IExecuteController
+
+        public void IExecute()
+        {
+            foreach (var ItemBehaviour in _context.GetListInteractable())
+            {
+                if (ItemBehaviour.IsInteractable)
+                {
+                    if (ItemBehaviour is ItemBehaviour interactable)
+                    {
+                        //
+                    }
+                }
+            }
+
+            #endregion
+
+        }
+    }
+}
